b029131d12cf34cdaaf9347e913ab038031a53df
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-03-01 20:39:32 -1000

    added additional corpus text

--- Code/format_text.py
+++ Code/format_text.py
@@ -7,7 +7,7 @@ def format_text(file):
     with open(file, "r") as file:
         for line in file:
             for word in line.split():
-                #strip the punctionation from both sides of the word & convert to lowercase
+                #strip the punctionation from both sides of the word & # edit- do not convert to lowercase
                 word = word.lstrip(punctuation).rstrip(punctuation).strip('\n').lower()
                 word_list.append(word)
 
